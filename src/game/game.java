1759e3ecc081816efc26232d9aa03531400c410a
Casey Manning <kcgrind@gmail.com>
2014-06-14 11:26:10 -0700

    Fixed play text

--- src/game/game.java
+++ src/game/game.java
@@ -6,16 +6,26 @@ import acm.program.GraphicsProgram;
 import acmx.export.javax.swing.JButton;
 
 public class game extends GraphicsProgram {
+	
 	public static final int APPLICATION_WIDTH = 670;
 	public static final int APPLICATION_HEIGHT = 474;
+	
 	public void run(){
+		
 		GImage Play = new GImage("button.png");
 		GImage background = new GImage("mBackground.jpg");
 		GLabel PlayText = new GLabel("Play");
+		
 		add(background);
+		
 		add(Play, (getWidth()/2) - (Play.getWidth()/2), (getHeight()/3) - (Play.getHeight()/2));
+		
 		PlayText.setColor(Color.BLACK);
-		add(PlayText, Play.getX()/* + (PlayText.getWidth()/2)*/, Play.getY()/* + (Play.getY()/2)*/);
-		println(Play.getLocation());
+		PlayText.setFont("UbuntuMono-BOLD-24");
+		add(PlayText, (getWidth()/2) - (PlayText.getWidth()/2), APPLICATION_HEIGHT/3 + (PlayText.getHeight()/3));
+		
+		println(PlayText.getLocation());
+		println(APPLICATION_WIDTH);
+		println(670/2);
 	}
 }
