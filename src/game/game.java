bf9b822a75d14bbfa375a3a73e27af97a6c7408f
bokken12 <stopwatchj@Joels-iMac.local>
2014-06-14 11:28:44 -0700

    Removed print statements

--- src/game/game.java
+++ src/game/game.java
@@ -15,15 +15,11 @@ public class game extends GraphicsProgram {
 		GImage Play = new GImage("button.png");
 		GImage background = new GImage("mBackground.jpg");
 		GLabel PlayText = new GLabel("Play");
-		
 		add(background);
-		
 		add(Play, (getWidth()/2) - (Play.getWidth()/2), (getHeight()/3) - (Play.getHeight()/2));
-		
 		PlayText.setColor(Color.BLACK);
 		PlayText.setFont("UbuntuMono-BOLD-24");
 		add(PlayText, (getWidth()/2) - (PlayText.getWidth()/2), APPLICATION_HEIGHT/3 + (PlayText.getHeight()/3));
-		
 		println(PlayText.getLocation());
 		println(APPLICATION_WIDTH);
 		println(670/2);
