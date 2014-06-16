2d6cee9db00eac2e8fe41ed85cb937bbc51205bd
bokken12 <stopwatchj@Joels-iMac.local>
2014-06-16 09:30:39 -0700

    First Draft of screen movement
    Still a little buggy but you can walk around any image and nothing also
    on the screen will go with you.

--- src/game/game.java
+++ src/game/game.java
@@ -7,7 +7,7 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class game extends GraphicsProgram {
-
+	gamescreen screen = new gamescreen(new GImage("BetterD&DMap.png"), 600, 600);
 	public static final int APPLICATION_WIDTH = 670;
 	public static final int APPLICATION_HEIGHT = 474;
 	GImage Play = new GImage("button.png");
@@ -17,10 +17,10 @@ public class game extends GraphicsProgram {
 	boolean up = false;
 	boolean down = false;
 	boolean playing = false;
-	
+
 	GImage background = new GImage("mBackground.jpg");
 	GLabel PlayText = new GLabel("Play");
-	GImage guy  = new GImage( "backwards0.png", 40,430);
+	GImage guy  = new GImage( "backwards0.png");
 	GRect solid = new GRect(250,250,27,170);
 	GRect peerson = new GRect(400, 100, 50,50);
 
@@ -46,27 +46,31 @@ public class game extends GraphicsProgram {
 	public void mousePressed(MouseEvent e) {
 		GObject click = getElementAt(e.getX(), e.getY());
 		if(playing == false) {
-		if(click.equals(Play) || click.equals(PlayText)) {
-			removeAll();
-			play();
-		}
+			if(click.equals(Play) || click.equals(PlayText)) {
+				removeAll();
+				play();
+			}
 		}
 		if(playing == true) {
 			GObject j = getElementAt(e.getX(), e.getY());
-			if(j.equals(peerson)) {
+			/*if(j.equals(peerson)) {
 				println("HI!");
-			}
+			}*/
 		}
 	}
 	public void play() {
 
 		addKeyListeners();
 		playing = true;
-		add(guy);
+		add(screen.getImage());
+		screen.register(guy);
+		add(guy, (getWidth()/2) - (guy.getWidth()/2), (getHeight()/2) - (guy.getHeight()/2));
+		screen.register(solid);
 		add(solid);
 
 		solid.setFilled(true);
 		solid.setFillColor(Color.GRAY);
+		screen.register(peerson);
 		add(peerson);
 
 	}
@@ -88,22 +92,28 @@ public class game extends GraphicsProgram {
 
 
 	public void walk(int key) {
+		
+		if(key == KeyEvent.VK_LEFT) {
 
-		GObject b = getElementAt(guy.getX() - 1, guy.getY());
+			GObject b = getElementAt(guy.getX() - 1, guy.getY());
 
-		if(b != solid) {
+			if(b != solid) {
 
-			GObject j = getElementAt(guy.getX() - 1, guy.getY() + guy.getHeight());
+				GObject j = getElementAt(guy.getX() - 1, guy.getY() + guy.getHeight());
 
-			if(j != solid) {
+				if(j != solid) {
 
-				GObject d = getElementAt(guy.getX() - 1, guy.getY() + (guy.getHeight()/2));
+					GObject d = getElementAt(guy.getX() - 1, guy.getY() + (guy.getHeight()/2));
 
-				if(d != solid) {
+					if(d != solid) {
 
-					if(key == KeyEvent.VK_LEFT) {
+
 						left = true;
 						guy.move(-1, 0);
+						remove(screen.getImage());
+						screen.left();
+						add(screen.getImage());
+						screen.moveObjectsToFront();
 						guy.setImage("left" + currentstate + ".png");
 					}
 				}
@@ -112,70 +122,85 @@ public class game extends GraphicsProgram {
 		}
 
 
+		if(key == KeyEvent.VK_RIGHT) {
+			
+			GObject a = getElementAt(guy.getX() + guy.getWidth() + 1,guy.getY());
 
-		GObject a = getElementAt(guy.getX() + guy.getWidth() + 1,guy.getY());
+			if(a != solid) {
 
-		if(a != solid) {
+				GObject f = getElementAt(guy.getX() + guy.getWidth() + 1,guy.getY() + guy.getHeight());
 
-			GObject f = getElementAt(guy.getX() + guy.getWidth() + 1,guy.getY() + guy.getHeight());
+				if(f != solid) {
 
-			if(f != solid) {
+					GObject m = getElementAt(guy.getX() + guy.getWidth() + 1,guy.getY() + (guy.getHeight()/2));
 
-				GObject m = getElementAt(guy.getX() + guy.getWidth() + 1,guy.getY() + (guy.getHeight()/2));
-
-				if(m != solid) {
+					if(m != solid) {
 
 
-					if(key == KeyEvent.VK_RIGHT) {
+
 						right = true;
 						guy.move(1, 0);
+						remove(screen.getImage());
+						screen.right();
+						add(screen.getImage());
+						screen.moveObjectsToFront();
 						guy.setImage("right" + currentstate + ".png");
 					}
 				}
 			}
 		}
 
+		if(key == KeyEvent.VK_UP) {
+			
+			GObject g = getElementAt(guy.getX() + (guy.getWidth()/2),guy.getY() - 1);
 
-		GObject g = getElementAt(guy.getX() + (guy.getWidth()/2),guy.getY() - 1);
-
-		if(g != solid) {
+			if(g != solid) {
 
 
-			GObject c = getElementAt(guy.getX(),guy.getY() - 1);
+				GObject c = getElementAt(guy.getX(),guy.getY() - 1);
 
-			if(c != solid) {
+				if(c != solid) {
 
-				GObject p = getElementAt(guy.getX() + guy.getWidth(),guy.getY() - 1);
+					GObject p = getElementAt(guy.getX() + guy.getWidth(),guy.getY() - 1);
 
-				if(p != solid) {
+					if(p != solid) {
 
-					if(key == KeyEvent.VK_UP) {
+
 						up = true;
 						guy.move(0, -1);
+						remove(screen.getImage());
+						screen.forward();
+						add(screen.getImage());
+						screen.moveObjectsToFront();
 						guy.setImage("backwards" + currentstate + ".png");
 					}
 				}
 			}
 		}
 
+		if(key == KeyEvent.VK_DOWN) {
+			
+			GObject l = getElementAt(guy.getX(),guy.getY() + guy.getHeight() + 1);
 
-		GObject l = getElementAt(guy.getX(),guy.getY() + guy.getHeight() + 1);
+			if(l != solid) {
 
-		if(l != solid) {
+				GObject n = getElementAt(guy.getX() + guy.getWidth(),guy.getY() + guy.getHeight() + 1);
 
-			GObject n = getElementAt(guy.getX() + guy.getWidth(),guy.getY() + guy.getHeight() + 1);
+				if(n != solid) {
 
-			if(n != solid) {
+					GObject k = getElementAt(guy.getX() + (guy.getWidth()/2),guy.getY() + guy.getHeight() + 1);
 
-				GObject k = getElementAt(guy.getX() + (guy.getWidth()/2),guy.getY() + guy.getHeight() + 1);
-
-				if(k != solid) {
+					if(k != solid) {
 
 
 
-					if(key == KeyEvent.VK_DOWN) {
+
 						down = true;
 						guy.move(0, 1);
+						remove(screen.getImage());
+						screen.backward();
+						add(screen.getImage());
+						screen.moveObjectsToFront();
 						guy.setImage("foward" + currentstate + ".png");
 					}
 				}
---
+++ src/game/gamescreen.java
@@ -0,0 +1,132 @@
+package game;
+import java.util.ArrayList;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class gamescreen extends GraphicsProgram{
+	ArrayList<GObject> objects = new ArrayList<GObject>();
+	GImage screenImage = new GImage("mBackground.jpg");
+	int APPLICATION_WIDTH = 670;
+	int APPLICATION_HEIGHT = 474;
+	int currentX = 0;
+	int currentY = 0;
+	int[][] newImage = new int[474][670];
+	int[][] imageArray;
+	public gamescreen(GImage inputImage, int xpos, int ypos){
+		imageArray = inputImage.getPixelArray();
+		currentX = xpos;
+		currentY = ypos;
+		newImage = getViewed();
+		screenImage = new GImage(getFlipped(newImage));
+		println("" + newImage.length + " " + newImage[0].length + " " + screenImage.getSize());
+	}
+	private int[][] getViewed(){
+		int[][] newpic = new int[474][670];
+		int newpicX = 0;
+		int newpicY = 0;
+		for(int x = currentX - 335; x < currentX + 335; x++, newpicX++){
+			for(int y = currentY - 237; y < currentY + 237; y++, newpicY++){
+				//println(" " + newpicX + " " + newpicY + " " + x + " " + y + " " + imageArray.length + " " + imageArray[1].length);
+				newpic[newpicY][newpicX] = imageArray[y][x];
+			}
+			newpicY = 0;
+		}
+		return newpic;
+	}/** Change the image onscreen*/
+	public void setImage(GImage inputImage, int xpos, int ypos){
+		remove(screenImage);
+		imageArray = inputImage.getPixelArray();
+		currentX = xpos;
+		currentY = ypos;
+		newImage = getViewed();
+		screenImage = new GImage(getFlipped(newImage));
+	}
+	public GImage getImage(){
+		return screenImage;
+	}
+	public void register(GObject object){
+		//add(object);
+		objects.add(object);
+	}
+	public void register(GObject object, int x, int y){
+		//add(object, x, y);
+		objects.add(object);
+	}
+	public void right(){
+		for(int x = 0; x < 669; x++){
+			for(int y = 0; y < 474; y++){
+				newImage[y][x] = newImage[y][x+1];
+			}
+		}
+		for(int y = 0; y < 474; y++){
+			newImage[y][669] = imageArray[(currentY + y) - 236][currentX + 335];
+		}
+		remove(screenImage);
+		screenImage = new GImage(getFlipped(newImage));
+		moveObjects(-1,0);
+		currentX++;
+	}
+	public void left(){
+		for(int x = 669; x > 0; x--){
+			for(int y = 0; y < 474; y++){
+				newImage[y][x] = newImage[y][x-1];
+			}
+		}
+		for(int y = 0; y < 474; y++){
+			newImage[y][0] = imageArray[(currentY + y) - 236][currentX - 334];
+		}
+		remove(screenImage);
+		screenImage = new GImage(getFlipped(newImage));
+		moveObjects(1,0);
+		currentX--;
+	}
+	public void forward(){
+		for(int x = 0; x < 670; x++){
+			for(int y = 473; y > 0; y--){
+				newImage[y][x] = newImage[y-1][x];
+			}
+		}
+		for(int x = 0; x < 670; x++){
+			newImage[0][x] = imageArray[currentY - 237][(currentX + x) - 334];
+		}
+		remove(screenImage);
+		screenImage = new GImage(getFlipped(newImage));
+		moveObjects(0,1);
+		currentY--;
+	}
+	public void backward(){
+		for(int x = 0; x < 670; x++){
+			for(int y = 0; y < 473; y++){
+				newImage[y][x] = newImage[y+1][x];
+			}
+		}
+		for(int x = 0; x < 670; x++){
+			newImage[473][x] = imageArray[currentY + 236][currentX + x - 334];
+		}
+		remove(screenImage);
+		screenImage = new GImage(getFlipped(newImage));
+		moveObjects(0,-1);
+		currentY++;
+	}
+	private void moveObjects(int xmove, int ymove){
+		for(int i = 0; i < objects.size(); i++){
+			objects.get(i).move(xmove, ymove);
+		}
+	}
+	public void moveObjectsToFront(){
+		for(int i = 0; i < objects.size(); i++){
+			objects.get(i).sendToFront();
+		}
+	}
+	public int[][] getFlipped(int[][] image){
+		int[][] oldImage = new int[474][670];
+		for(int x = 0; x < 670; x++){
+			for(int y = 0; y < 474; y++){
+				oldImage[y][x] = image[y][x];
+			}
+		}
+		return oldImage;
+		}
+
+}
